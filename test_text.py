a5e4759d135933a94d94980ba36706cacd558e8b
Álvaro Marques Macedo <sc.poi458@gmail.com>
2016-07-30 13:22:04 -0300

    Add files via upload

--- test_text.py
+++ test_text.py
@@ -1,7 +1,8 @@
-from re import findall
+from re import findall,sub
 from random import randint
+from sys import argv
 def findWords(word,text):
-        f = findall(r"%s([\W]+\w+)"%word,text)
+        f = findall(r"%s([\W]+\w+)"%sub("\W+",'',word),text)
         l = len(f)-1
         return f[randint(0,l)] if l != -1 else None
 file = open("input.txt","r")
@@ -9,12 +10,14 @@ read = file.read()
 file.close()
 del file
 text = ''
-word = 'kill'
-for i in range(100):
+word = argv[1] if len(argv)>=1 else 'fight'
+for i in range(100 if  len(argv) < 2 else int(argv[2])):
         if not word: break
         text +='%s'%word
         word = findWords(word,read)
-        
+
+print(text)        
 out = open("output.txt","w")
 out.write(text)
 out.close()
+
